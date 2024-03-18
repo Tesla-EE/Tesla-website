99c10fc1164d5a2b725d8ba9a942ca529b5360d4
MrWonder2 <bharathpadmanabh2005@gmail.com>
2024-03-18 21:48:47 +0530

    Minor Changes: Copyright changes, Footer Changes, Smooth Scroll

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,45 @@
+(function ($) {
+	"use strict";
 
-  (function ($) {
-  
-  "use strict";
+	// MENU
+	$(".navbar-collapse a").on("click", function () {
+		$(".navbar-collapse").collapse("hide");
+	});
 
-    // MENU
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
-    });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-      var el = $(this).attr('href');
-      var elWrapped = $(el);
-      var header_height = $('.navbar').height();
-  
-      scrollToDiv(elWrapped,header_height);
-      return false;
-  
-      function scrollToDiv(element,navheight){
-        var offset = element.offset();
-        var offsetTop = offset.top;
-        var totalScroll = offsetTop-navheight;
-  
-        $('body,html').animate({
-        scrollTop: totalScroll
-        }, 300);
-      }
-    });
-  
-  })(window.jQuery);
+	// CUSTOM LINK
+	$(".smoothscroll").click(function () {
+		var el = $(this).attr("href");
+		var elWrapped = $(el);
+		var header_height = $(".navbar").height();
 
+		scrollToDiv(elWrapped, header_height);
+		return false;
 
+		function scrollToDiv(element, navheight) {
+			var offset = element.offset();
+			var offsetTop = offset.top;
+			var totalScroll = offsetTop - navheight;
+
+			$("body,html").animate(
+				{
+					scrollTop: totalScroll,
+				},
+				300
+			);
+		}
+	});
+
+	// SMOOTH SCROLL
+	$(function () {
+		$(".smoothscroll").on("click", function (e) {
+			e.preventDefault();
+			$("html, body").animate(
+				{
+					scrollTop: $($(this).attr("href")).offset().top,
+				},
+				1000,
+				"easeInOutExpo"
+			);
+		});
+	});
+})(window.jQuery);
