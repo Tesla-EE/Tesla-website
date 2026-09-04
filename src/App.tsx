0a5b99870f442a4722031d5328856009ed7f6794
acadamicsupportcell <pisense.in@gmail.com>
2026-09-04 17:36:29 +0530

    fix: Corrected sitemap.xml - was truncated causing Google Search Console error

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import Home from './pages/Home';
-import ComingSoon from './pages/ComingSoon';
-import './styles/globals.css';
-
-export default function App() {
-  // Temporarily showing ComingSoon
-  return <ComingSoon />;
-}
